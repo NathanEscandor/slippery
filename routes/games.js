183ee4d0e85401b417515b854cc9b2b9ac50b54a
NathanEscandor <nathan.escandor@gmail.com>
2019-10-24 11:44:30 -0600

    Games now save to mongodb
    Still using shit variables and logging but it works haha

--- routes/games.js
+++ routes/games.js
@@ -19,7 +19,7 @@ router.get('/:id', async(req, res) => {
   res.send(game);
 });
 
-router.post('/uploads', upload.single('slp_file'), (req, res) => {
+router.post('/uploads', upload.single('slp_file'), async(req, res) => {
   try {
     res.send(req.file);
   } catch (err) {
@@ -31,13 +31,28 @@ router.post('/uploads', upload.single('slp_file'), (req, res) => {
   const server_response = res.send(req.files);
 
   const test_game = new SlippiGame(server_response.req.file.path);
-  console.log("stats:");
-  console.log(test_game.getStats());
+  // console.log("stats:");
+  // console.log(test_game.getStats());
 
-  console.log("metadata:");
-  console.log(test_game.getMetadata());
+  // console.log("metadata:");
+  // console.log(test_game.getMetadata());
   // console.log("after the send?");
   // console.log(test_game.stats);
+
+  const slp_settings = test_game.getSettings();
+  const slp_metadata = test_game.getMetadata();
+  const slp_stats = test_game.getStats();
+
+  let game = new Game({
+    name: req.body.name || "current_time",
+    settings: slp_settings,
+    metadata: slp_metadata,
+    stats: slp_stats
+  });
+
+  game = await game.save();
+  console.log(game);
+
 
 });
 
