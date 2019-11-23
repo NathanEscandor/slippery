bbdfd14674b4a2e6a4d18968fca06e4dd4bef472
NathanEscandor <nathan.escandor@gmail.com>
2019-11-23 16:16:05 -0700

    Added module and util files for mongodb

---
+++ slippery-server/modules/mongodb/mongodb.module.js
@@ -0,0 +1,7 @@
+(function () {  //IIFE block to private the variables
+  'use strict';
+
+  module.exports = {
+    //here, we declar attributes that will be exposed to other modules
+  };
+}) ();
---
+++ slippery-server/modules/mongodb/mongodb.util.js
@@ -0,0 +1,7 @@
+(function () {
+  'use strict';
+
+  module.exports = {
+    //declare attrs to be exposed to other modules
+  };
+}) ();
