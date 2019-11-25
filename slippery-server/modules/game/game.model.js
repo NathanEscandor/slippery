6e080105ec465d426dcd014912dc22baab28285e
NathanEscandor <nathan.escandor@gmail.com>
2019-11-25 00:18:54 -0700

    Filled in the game model
    Note: not yet doing validation on input. Maybe look into a package like Joi to make this easier?

--- slippery-server/modules/game/game.model.js
+++ slippery-server/modules/game/game.model.js
@@ -3,7 +3,34 @@
   const Schema = mongoose.Schema;
 
   const GameSchema = new Schema({
-
+    name: {
+      type: String,
+      required: true
+    },
+    dateCreated: {
+      type: Date,
+      default: Date.now
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false
+    },
+    tags: [ String ],
+    path: {
+      type: String
+    },
+    settings: {
+      type: Object,
+      required: true
+    },
+    metadata: {
+      type: Object,
+      required: true
+    },
+    stats: {
+      type: Object,
+      required: true
+    }
   });
   
   module.exports = mongoose.model('games', GameSchema);
