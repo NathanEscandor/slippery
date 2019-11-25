3c951028226dd2f73155fd33408c14ec6d3f8435
NathanEscandor <nathan.escandor@gmail.com>
2019-11-25 12:49:17 -0700

    Added middleware add game functionality and test

--- slippery-server/modules/game/game.middleware.js
+++ slippery-server/modules/game/game.middleware.js
@@ -1,5 +1,18 @@
 (function() {
   'use strict';
 
-  module.exports = {};
+  module.exports = {
+    addGame: addGame
+  };
+  const GameService = require('./game.module')().GameService;
+
+  function addGame(req, res, next) {
+    GameService.createGame(req.body)
+      .then(success);
+
+    function success(data) {
+      req.response = data;
+      next();
+    }
+  }
 }) ();
---
+++ slippery-server/tests/unit/game/game.middleware.spec.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const httpMocks = require('node-mocks-http');
+
+const bluebird = require('bluebird');
+const Promise = bluebird.Promise;
+
+const GameModule = require('../../../modules/game/game.module')();
+const GameMiddleware = GameModule.GameMiddleware;
+const GameService = GameModule.GameService;
+
+const Fixtures = require('../../fixtures/fixtures');
+const GameFixture = Fixtures.GameFixture;
+const ErrorFixture = Fixtures.ErrorFixture;
+
+let req, res, next;
+
+describe('GameMiddleware', function () {
+  beforeEach(function() {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = sinon.spy();
+  });
+
+  describe('addGame', function() {
+    let createGame, createGamePromise, expectedCreatedGame, expectedError;
+
+    beforeEach(function () {
+      createGame = sinon.stub(GameService, 'createGame');
+      req.body = GameFixture.newGame;
+    });
+
+    afterEach(function () {
+      createGame.restore();
+    })
+
+    it('should successfully create new game', function() {
+      expectedCreatedGame = GameFixture.createdGame;
+      createGamePromise = Promise.resolve(expectedCreatedGame);
+      createGame.withArgs(req.body).returns(createGamePromise);
+
+      GameMiddleware.addGame(req, res, next);
+
+      sinon.assert.callCount(createGame, 1);
+
+      return createGamePromise.then(function () {
+        expect(req.response).to.be.a('object');
+        expect(req.response).to.deep.equal(expectedCreatedGame);
+        sinon.assert.callCount(next, 1);
+      });
+    });
+    
+  });
+});
