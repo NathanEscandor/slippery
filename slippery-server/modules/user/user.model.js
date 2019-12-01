6b6a9c1a016c38d664cc5c5bb24cf4271f40a39f
NathanEscandor <nathan.escandor@gmail.com>
2019-11-30 18:26:26 -0700

    Added bcrypt

--- slippery-server/modules/user/user.model.js
+++ slippery-server/modules/user/user.model.js
@@ -1,5 +1,6 @@
 (function() {
   const mongoose = require('mongoose');
+  const bcrypt = require('bcryptjs');
   const Schema = mongoose.Schema;
 
   const UserSchema = new Schema({
@@ -32,6 +33,20 @@
       default: Date.now,
     }
   });
+
+  UserSchema.pre('save', function(next) {
+    let user = this;
+    //need to add a case for if password is not modified
+
+    //gensalt and hash
+    if (user.password) {
+      bcrypt.hash(user.password, 10, function (err, hash) {
+        if (err) return next(err); 
+          user.password = hash;
+          next();
+      })
+    }
+  })
 
   module.exports = mongoose.model('users', UserSchema);
 }) (); 
